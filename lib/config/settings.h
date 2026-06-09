72feba955b4f28bec690bea329c540d005339ec3
Akram Gibril <Ahmed Kamil>
2026-06-09 17:59:04 -0400

    Modificacion total de la estructura

--- lib/config/settings.h
+++ lib/config/settings.h
@@ -1,12 +1,11 @@
 #pragma once
 #define SETTINGS_MAGIC 0x20260509
 
-struct Settings {
-
-    uint32_t magic;
-
+struct Settings
+{
     char wifiSSID[32];
 
     char wifiPassword[64];
 
+    char hostname[32];
 };
